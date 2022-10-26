919d55a50e95fd4686306c6e82c3371019bb11f8
Ilari Tuominen <ilari.tuominen@kodan.fi>
2022-10-26 10:56:52 +0300

    Create App.tsx

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react"
+
+type Props = {}
+
+const App: FC<Props> = props => {
+  return <main>
+    <h1>Sarastia 180 Agile Business Enterprise Resource Planner</h1>
+  </main>
+}
+
+export default App
